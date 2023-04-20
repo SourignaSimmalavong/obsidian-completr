8a82dd9c92539dcdc9fee589cad703391b57c2c7
ssimmalavong <sourigna.simmalavong@gmail.com>
2023-04-20 17:36:32 +0200

    Make it handle asian symbols and some special characters.

--- src/provider/dictionary_provider.ts
+++ src/provider/dictionary_provider.ts
@@ -42,7 +42,27 @@ export abstract class DictionaryProvider implements SuggestionProvider {
 
         // Queries like vscode smart completion:
         // any word with `query` characters in the right order (even if not adjacent) will match
-        let fullq = new RegExp(query.replace(/.{1}/g, "$&"+"\\w*"));
+
+        let cjk_range: string = '\u4e00-\u9fff'
+        let cjk_strokes: string = '\u31c0-\u31ef'
+        let katakana_phonetic_extension: string = '\u31f0-\u31ff'
+        let Enclosed_CJK_Letters_and_Months: string = '\u3200-\u32ff'
+        let CJK_Compatibility: string = '\u3300-\u33ff'
+        let CJK_Unified_Ideographs_Extension_A: string = '\u3400-\u4dbf'
+        let Yijing_Hexagram_Symbols: string = '\u4dc0-\u4dff'
+        const asian_symbols: string = cjk_range + cjk_strokes + katakana_phonetic_extension + Enclosed_CJK_Letters_and_Months + CJK_Compatibility + CJK_Unified_Ideographs_Extension_A + Yijing_Hexagram_Symbols
+
+        let esc_query: string = query;
+
+        esc_query = esc_query.replace(/.{1}/gu, "$&"+"[\\w\\s" + asian_symbols + "()]*"); // parenthesis will get escaped just after
+
+        const special_characters: Array<string> = ['(', ')', '^', '$', '.', '|', '?', '+']; // '[', ']', '\\', '*',
+        for (let special_character of special_characters)
+        {
+            esc_query = esc_query.replaceAll(special_character, "\\" + special_character);
+        }
+
+        let fullq = new RegExp(esc_query);
 
         const result: Suggestion[] = [];
         for (let el of list) {
